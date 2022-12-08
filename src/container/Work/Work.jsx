ba109d0d2541981167a16b2c5d28e74dd1003a51
RichGod <usenrichgod93@gmail.com>
2022-12-08 19:46:27 +0100

    updated works

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -44,19 +44,17 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Next JS", "React JS", "Supabase", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
+        {["Next JS", "React JS", "Supabase", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
