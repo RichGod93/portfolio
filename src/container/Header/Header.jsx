8fa33ed13393ee60255483ea7faca027d5110d2e
RichGod <usenrichgod93@gmail.com>
2022-12-10 09:36:20 +0100

    header circle update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.firebase, images.react, images.node, images.Photoshop].map(
+        {[images.firebase, images.react, images.node, images.git].map(
           (circle, index) => (
             <div className="circle-cmp app__flex" key={`curcke-${index}`}>
               <img src={circle} alt="circle" />
